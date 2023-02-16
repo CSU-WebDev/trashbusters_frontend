0e5158b31e2018b464d9d5656ce603473f1e3bd3
farmermichael1 <111703957+farmermichael1@users.noreply.github.com>
2023-02-16 18:11:06 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Footer() {
     const currentYear = new Date().getFullYear();
