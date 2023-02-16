a70f6b511b018a25fd5564fcfff51b6723d4d20d
farmermichael1 <111703957+farmermichael1@users.noreply.github.com>
2023-02-16 18:10:27 -0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Contact() {
     return (
-        <p>Contact</p>
+        <>
+        <Container fluid class="center" className="mt-2 mb-2">
+            <div class="box">
+                <Row>
+                    <Col><h3 class="text-center" id="contact">Contact</h3></Col>
+                    </Row>
+                    <Row>
+                    <Col><p>I'm ed we here at ed like our ed that is ed I'm ed we here at ed like our ed that is ed I'm ed we here at ed like our ed that is edI'm ed we here at ed like our ed that is ed I'm ed we here at ed like our ed that is ed</p></Col>
+                </Row>
+            </div>
+            </Container>
+
+        </>
     )
 }
