aae579b056a143e7504130e8d8fea5a06627169b
JoseJCanedo <canedo_jose@columbusstate.edu>
2023-02-24 05:15:37 +0000

    vf

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import './App.css'
 import NavigationBar from './components/Navbar.jsx'
 import Main from './components/Main.jsx'
 import Footer from './components/Footer.jsx'
+import axios from 'axios'
 
 function App() {
-
+  axios.get('https://se4r56t7yg.onrender.com/api/getAll')
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
   return (
     <div className="App">
       <NavigationBar />
