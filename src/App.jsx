85a25783e1e0b4515213e0ae35e79b260b89b55d
austinProGit <97987573+austinProGit@users.noreply.github.com>
2023-03-09 18:19:51 -0500

    removed extra code

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,8 @@ import './App.css'
 import NavigationBar from './components/Navbar.jsx'
 import Main from './components/Main.jsx'
 import Footer from './components/Footer.jsx'
-import axios from 'axios'
 
 function App() {
-  axios.get('https://trashbusters-backend.onrender.com')
-  .then(function (response) {
-    console.log(response.data);
-  })
   return (
     <div className="App">
       <NavigationBar />
